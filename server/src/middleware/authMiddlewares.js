8aaff1c465e3f33cf607b2a87c975adbe86776ee
Dima <my_email@gmail.com>
2025-09-09 01:32:07 +0300

    feat(auth): add middlewar for check access token

--- server/src/middleware/authMiddlewares.js
+++ server/src/middleware/authMiddlewares.js
@@ -1,11 +1,21 @@
+const { verifyToken } = require('../services/tokenService');
+
 module.exports.checkAccessToken = async (req, res, next) => {
- try {
-    const { headers: { authorization } } = req;
-    if (!authorization) {
+  try {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
       return res.status(401).send({ error: "Access token is required" });
     }
+
+    const [type, token] = authHeader.split(' ');
+    if (type !== 'Bearer' || !token) {
+      return res.status(401).send({ error: "Invalid authorization header format" });
+    }
+
+    const decoded = await verifyToken(token);
+    req.user = decoded;
     next();
   } catch (error) {
-      next(error);
-    }
+    next(error);
+  }
 };
